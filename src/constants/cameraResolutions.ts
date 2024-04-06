e0f5039530c39e8949e300fe853e2f66ad798d0d
Aditya Singh <aditya.singh@anyline.com>
2024-04-06 19:44:35 +0200

    CAPI-145(chore): add ImageCapture API support, get image blob and support for navigator.mediaDevices

---
+++ src/constants/cameraResolutions.ts
@@ -0,0 +1,13 @@
+const cameraResolutions = [
+	{ name: 'Highest Resolution', width: 99999, height: 99999 },
+	{ name: '8K Ultra HD', width: 7680, height: 4320 },
+	{ name: '5K', width: 5120, height: 2880 },
+	{ name: '4K Ultra HD', width: 3840, height: 2160 },
+	{ name: '1440p QHD', width: 2560, height: 1440 },
+	{ name: '2K', width: 2048, height: 1080 },
+	{ name: '720p HD', width: 1280, height: 720 },
+	{ name: '1080p Full HD', width: 1920, height: 1080 },
+	{ name: 'Default', width: 640, height: 480 },
+];
+
+export default cameraResolutions;
--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,124 @@
 // 7. Use ImageCapture polyfill from npm
 // 8. Check why importing the function doesn't work on browser
 
-const getImage = async ({
-	videoElementId,
-}: {
-	videoElementId: string;
-}): Promise<void> => {
-	if (typeof navigator.mediaDevices === 'undefined') {
+// @ts-expect-error - No type definitions available for image-capture.
+import { ImageCapture } from 'image-capture';
+
+import {
+	backCameraKeywords,
+	iosWideBackCameraLabels,
+} from './constants/cameraLabels';
+import cameraResolutions from './constants/cameraResolutions';
+
+function isBackCameraLabel(label: string): boolean {
+	const lowercaseLabel = label.toLowerCase();
+	return backCameraKeywords.some(keyword => lowercaseLabel.includes(keyword));
+}
+
+function isNotIosWideAngleCamera(label: string): boolean {
+	const lowercaseLabel = label.toLowerCase();
+	return !iosWideBackCameraLabels.some(iosLabel =>
+		lowercaseLabel.includes(iosLabel.toLowerCase())
+	);
+}
+
+async function getHighResolutionNonWideAngleCamera(): Promise<MediaDeviceInfo> {
+	const allDevices = await navigator.mediaDevices.enumerateDevices();
+
+	const videoDevices = allDevices.filter(
+		device => device.kind === 'videoinput'
+	);
+
+	if (videoDevices?.length === 0) {
+		return await Promise.reject(new Error('No video device found'));
+	}
+
+	let backCameras = videoDevices.filter(
+		device =>
+			isBackCameraLabel(device.label) &&
+			isNotIosWideAngleCamera(device.label)
+	);
+
+	backCameras = backCameras.reverse();
+
+	let chosenCamera: MediaDeviceInfo;
+
+	if (backCameras.length > 0) chosenCamera = backCameras[0];
+	else chosenCamera = videoDevices[0];
+
+	return chosenCamera;
+}
+
+async function getHighestResolutionStream(
+	device: MediaDeviceInfo
+): Promise<MediaStream> {
+	const constraintsList = cameraResolutions.map(resolution => ({
+		video: {
+			deviceId:
+				device !== undefined && device !== null
+					? { exact: device.deviceId }
+					: undefined,
+			width: { ideal: resolution.width },
+			height: { ideal: resolution.height },
+			facingMode: 'environment',
+		},
+	}));
+
+	for (let i = 0; i < constraintsList.length; i++) {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia(
+				constraintsList[i]
+			);
+			if (stream !== null && stream !== undefined) return stream;
+		} catch (error) {
+			console.log(
+				`Attempt for resolution ${constraintsList[i].video.width.ideal}x${constraintsList[i].video.height.ideal} failed:`,
+				error
+			);
+		}
+	}
+
+	return await Promise.reject(Error('No suitable constraints found'));
+}
+
+async function getImageBlob(stream: MediaStream): Promise<Blob> {
+	const track = stream.getVideoTracks()[0];
+	const imageCaptureInstance = new ImageCapture(track);
+	const photoCapabilities = await imageCaptureInstance.getPhotoCapabilities();
+
+	const maxImageWidth = photoCapabilities.imageWidth.max;
+	const maxImageHeight = photoCapabilities.imageHeight.max;
+
+	const blob = await imageCaptureInstance.takePhoto({
+		imageHeight: maxImageHeight,
+		imageWidth: maxImageWidth,
+	});
+	return blob;
+	// onImageCapture(blob);
+}
+
+// Get the right media stream from highest resolution camera
+async function getMedia(videoElementId: string): Promise<Blob> {
+	const device = await getHighResolutionNonWideAngleCamera();
+	const stream = await getHighestResolutionStream(device);
+	const video = document.getElementById(
+		videoElementId
+	) as HTMLVideoElement | null;
+	if (video !== null && video !== undefined) {
+		video.srcObject = stream;
+	}
+	return await getImageBlob(stream);
+}
+
+export const getImage = async (videoElementId: string): Promise<void> => {
+	if (
+		navigator.mediaDevices === null ||
+		navigator.mediaDevices === undefined
+	) {
 		await Promise.reject(new Error('Unsupported device'));
 	}
 	const videoElement = document.getElementById(videoElementId);
+	console.log('v', videoElement);
 	if (videoElement === null || videoElement === undefined) {
 		await Promise.reject(
 			new Error(
@@ -24,8 +133,7 @@ const getImage = async ({
 		);
 	}
 	try {
-		// TODO
-		// await Promise.resolve('Success');
+		await getMedia(videoElementId);
 	} catch (error) {
 		if (error instanceof Error) {
 			console.error(error.message);
@@ -33,5 +141,7 @@ const getImage = async ({
 		await Promise.reject(error);
 	}
 };
+
+(window as any).Anyline = { getImage };
 
 export default getImage;
