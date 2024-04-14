439558383cd38fb8253a229c00e34d7cc680641f
Aditya Singh <aditya.singh@anyline.com>
2024-04-14 17:48:03 +0200

    CAPI-145(fix): fix video stream not being displayed in iphone 11

--- src/video/index.ts
+++ src/video/index.ts
@@ -5,12 +5,14 @@ export default function createVideoElementWithStream(
 	stream: MediaStream
 ): void {
 	const video = document.createElement('video');
-	video.className = css.video;
 	video.setAttribute('data-test-id', 'videoElement');
 	video.autoplay = true;
 	video.playsInline = true;
 	video.muted = true;
 	video.srcObject = stream;
+	setTimeout(() => {
+		video.className = css.video;
+	}, 300);
 
 	container.appendChild(video);
 }
