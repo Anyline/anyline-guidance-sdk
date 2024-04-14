74425248321e45452dbed64fceec3239bdcfeb7b
Aditya Singh <aditya.singh@anyline.com>
2024-04-14 11:22:01 +0200

    CAPI-194(test): test to check if sdk return image blob after clicking capture

---
+++ tests/integration/browserstack/index.test.ts
@@ -0,0 +1,86 @@
+import { expect } from 'expect-webdriverio';
+
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+
+// test cases
+// 1. Check when start sdk button is clicked, it should ask for permission
+// 2. When allow is clicked, it should open a modal which must have a video element and capture button
+// 3. When capture button is clicked, the text inside should change to "Please wait..."
+// 4. When capture button is clicked, after some time, img element should have src defined
+
+async function visitDemo(): Promise<void> {
+	// await browser.url('https://cloud-api-user-guidance-app.vercel.app/');
+	await browser.url('http://localhost:8000');
+}
+
+async function startSDK(): Promise<void> {
+	const startSdkButton = await $('#start-sdk-btn');
+	await startSdkButton.click();
+}
+
+async function acceptCameraPermission(): Promise<void> {
+	const contexts = await browser.getContexts();
+	await browser.switchContext(contexts[0] as string);
+	let popUp;
+	if (browser.isAndroid) {
+		popUp = await $('.//android.widget.Button[@text="Allow"]');
+	}
+	if (browser.isIOS) {
+		const capabilities = await browser.capabilities;
+		const { browserName } = capabilities;
+		if (browserName === '') {
+			// hack for chromium on ios, browsername is empty for chromium on ios
+			// it shows 1 popup before the native permission popup
+			// accept the 1st popup first
+			const chormiumPopUp = await $(`[name="OK"]`);
+			await chormiumPopUp.click();
+		}
+		popUp = await $(`[name="Allow"]`);
+	}
+	await popUp.click();
+	await browser.switchContext(contexts[1] as string);
+}
+
+describe('guidance sdk', () => {
+	it.skip('should open modal with video element and capture button', async () => {
+		await visitDemo();
+		await startSDK();
+		await acceptCameraPermission();
+
+		const videoElement = await $('*[data-test-id="videoElement"]');
+		const captureButton = await $('*[data-test-id="captureButton"]');
+		await expect(videoElement).toBeDisplayed();
+		await expect(captureButton).toBeDisplayed();
+	});
+
+	it('should receive image blob after capture button is clicked', async () => {
+		await visitDemo();
+
+		const demoImage = await $('#new-image');
+
+		const demoImageSrcBefore = await demoImage.getAttribute('src');
+
+		await expect(demoImageSrcBefore).toBeFalsy();
+
+		await startSDK();
+		await acceptCameraPermission();
+
+		const captureButton = await $('*[data-test-id="captureButton"]');
+		await captureButton.click();
+
+		await browser.waitUntil(
+			async () => {
+				const src = await demoImage.getAttribute('src');
+				return src && src.length > 0;
+			},
+			{
+				timeout: 5000,
+				timeoutMsg: 'Image src was not set within the expected time',
+			}
+		);
+
+		const demoImageSrcAfter = await demoImage.getAttribute('src');
+
+		await expect(demoImageSrcAfter).toBeTruthy();
+	});
+});
