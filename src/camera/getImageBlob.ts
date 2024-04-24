1579b09c2e4c37d72ded16ffa810a9ec57af19e2
Aditya Singh <aditya.singh@anyline.com>
2024-04-24 08:32:56 +0200

    CAPI-145(fix): fix capture event not returning image blob on iphone devices

--- src/camera/getImageBlob.ts
+++ src/camera/getImageBlob.ts
@@ -1,21 +1,25 @@
 export async function getImageBlob(): Promise<Blob> {
-	return await new Promise((resolve, reject) => {
-		const inputElement = document.createElement('input');
-		inputElement.type = 'file';
-		inputElement.accept = 'image/*';
-		inputElement.capture = 'camera';
-		inputElement.click();
+	const inputElement = document.createElement('input');
+	inputElement.type = 'file';
+	inputElement.accept = 'image/*';
+	inputElement.capture = 'camera';
+	inputElement.click();
 
-		inputElement.onchange = function (event) {
+	document.body.appendChild(inputElement);
+
+	return await new Promise((resolve, reject) => {
+		inputElement.addEventListener('change', event => {
 			const fileInput = event.target as HTMLInputElement;
 			if (fileInput.files != null && fileInput.files.length > 0) {
 				const file = fileInput.files[0];
 				if (file !== undefined && file !== null) {
+					document.body.removeChild(inputElement);
 					resolve(file);
 				} else {
+					document.body.removeChild(inputElement);
 					reject(new Error('No file selected'));
 				}
 			}
-		};
+		});
 	});
 }
