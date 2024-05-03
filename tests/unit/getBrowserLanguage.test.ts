24b3dfe2e7af90ce31970254437a9df0af1827c7
Aditya Singh <aditya.singh@anyline.com>
2024-05-03 11:55:16 +0200

    CAPI-145(test): add test for getBrowserLanguage

---
+++ tests/unit/getBrowserLanguage.test.ts
@@ -0,0 +1,51 @@
+import getBrowserLanguage from '../../src/lib/getBrowserLanguage';
+
+describe('getBrowserLanguage', () => {
+	afterEach(() => {
+		jest.restoreAllMocks();
+	});
+
+	it('returns english as default language when navigator.language is undefined', async () => {
+		Object.defineProperty(global.navigator, 'language', {
+			writable: true,
+			value: undefined,
+		});
+
+		const browserLanguage = getBrowserLanguage();
+
+		await expect(browserLanguage).toBe('english');
+	});
+
+	it('returns enslish when browser language is english', async () => {
+		Object.defineProperty(global.navigator, 'language', {
+			writable: true,
+			value: 'en',
+		});
+
+		const browserLanguage = getBrowserLanguage();
+
+		await expect(browserLanguage).toBe('english');
+	});
+
+	it('returns german when browser language is german', async () => {
+		Object.defineProperty(global.navigator, 'language', {
+			writable: true,
+			value: 'de',
+		});
+
+		const browserLanguage = getBrowserLanguage();
+
+		await expect(browserLanguage).toBe('german');
+	});
+
+	it('returns german when browser language is austrian german', async () => {
+		Object.defineProperty(global.navigator, 'language', {
+			writable: true,
+			value: 'de-AT',
+		});
+
+		const browserLanguage = getBrowserLanguage();
+
+		await expect(browserLanguage).toBe('german');
+	});
+});
