a763e7a3f416c0ca4188a3891e33c67229a4f846
Aditya Singh <aditya.singh@anyline.com>
2024-05-07 16:58:34 +0200

    CAPI-217(feat): add onboarding instructions component

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -2,7 +2,7 @@ import css from './index.module.css';
 
 export default function createModal(
 	shadowRoot: HTMLDivElement | ShadowRoot
-): HTMLElement {
+): HTMLDivElement {
 	const modal = document.createElement('div');
 	modal.className = css.modal;
 	modal.setAttribute('data-testid', 'components-modal');
