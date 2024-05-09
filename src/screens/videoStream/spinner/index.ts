b0dcf5cbde87b6ebe5ba501baff7bef002053abf
Aditya Singh <aditya.singh@anyline.com>
2024-05-09 17:32:26 +0200

    CAPI-217(feat): add spinner to show loading state when video stream is being requested

--- src/screens/videoStream/spinner/index.ts
+++ src/screens/videoStream/spinner/index.ts
@@ -1,6 +1,13 @@
+import css from './index.module.css';
+
 export default function createSpinner(): HTMLDivElement {
 	const spinnerWrapper = document.createElement('div');
-	spinnerWrapper.innerHTML = 'Loading';
+	spinnerWrapper.className = css.spinnerWrapper;
+
+	const spinner = document.createElement('div');
+	spinner.className = css.spinner;
+
+	spinnerWrapper.appendChild(spinner);
 
 	return spinnerWrapper;
 }
