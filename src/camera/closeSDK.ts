9b65e006fc930361ef211092dba6d8fa41f278bb
Aditya Singh <aditya.singh@anyline.com>
2024-05-09 17:01:54 +0200

    CAPI-217(feat): update closeSDK to close all instance of classes and remove host

--- src/camera/closeSDK.ts
+++ src/camera/closeSDK.ts
@@ -1,11 +1,24 @@
-import createShadowRoot from '../lib/createShadowRoot';
-import createHost from '../lib/createHost';
+import FileInputManager from '../modules/FileInputManager';
+import ImageManager from '../modules/ImageManager';
+import Router from '../modules/Router';
 import StreamManager from '../modules/StreamManager';
+import VideoManager from '../modules/VideoManager';
 
 export default function closeSDK(): void {
-	const host = createHost();
-	const shadowRoot = createShadowRoot(host);
-	shadowRoot.innerHTML = '';
 	const streamManager = StreamManager.getInstance();
+	const videoManager = VideoManager.getInstance();
+	const fileInputManager = FileInputManager.getInstance();
+	const imageManager = ImageManager.getInstance();
+	const routerManager = Router.getInstance();
+
 	streamManager.destroy();
+	videoManager.destroy();
+	fileInputManager.destroy();
+	imageManager.destroy();
+	routerManager.destroy();
+
+	const host = document.getElementById('anyline-guidance-sdk');
+	if (host !== null) {
+		document.body.removeChild(host);
+	}
 }
