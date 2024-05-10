9028dd76d93cb486459f3e7a3458785636b59192
Aditya Singh <aditya.singh@anyline.com>
2024-05-10 09:35:14 +0200

    CAPI-217(test): remove injectCSS test as the SDK now requires user click to resolve

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,4 @@
 import { readFileSync } from 'fs';
-import { jest } from '@jest/globals';
-
-jest.mock('./src/lib/injectCSS');
 
 beforeEach(() => {
 	const html = readFileSync('public/index.html', 'utf8');
--- tests/unit/init.test.ts
+++ tests/unit/init.test.ts
@@ -1,5 +1,4 @@
 import init from '../../src/index';
-import injectCSS from '../../src/lib/injectCSS';
 
 describe('init', () => {
 	afterEach(() => {
@@ -9,25 +8,5 @@ describe('init', () => {
 
 	it('rejects for unsupported devices', async () => {
 		await expect(init()).rejects.toThrow('Unsupported device');
-	});
-
-	it('calls injectCSS when init is called in production build', async () => {
-		process.env.MODE = 'production';
-		Object.defineProperty(global.navigator, 'mediaDevices', {
-			writable: true,
-			value: {},
-		});
-		try {
-			await init();
-		} catch (err) {}
-		void expect(injectCSS).toHaveBeenCalledTimes(1);
-	});
-
-	it('does not call injectCSS when init is called in development build', async () => {
-		process.env.MODE = 'development';
-		try {
-			await init();
-		} catch (err) {}
-		void expect(injectCSS).toHaveBeenCalledTimes(0);
 	});
 });
