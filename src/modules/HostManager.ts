508dfd7148e4d40ffdf6cf8a7c06a44e12f7088d
Aditya Singh <aditya.singh@anyline.com>
2024-05-10 14:22:30 +0200

    CAPI-217(feat): add HostManager to manage host and shadowRoot of the SDK

---
+++ src/modules/HostManager.ts
@@ -0,0 +1,35 @@
+export default class HostManager {
+	private static instance: HostManager | null = null;
+	private readonly host: HTMLDivElement = document.createElement('div');
+	private shadowRoot: ShadowRoot | HTMLDivElement | null = null;
+
+	constructor() {
+		const hostId = 'anyline-guidance-sdk';
+		const host = document.getElementById(hostId);
+		if (host === null || host === undefined) {
+			this.host.id = hostId;
+			this.host.setAttribute('data-testid', 'modules-hostmanager');
+			document.body.appendChild(this.host);
+		}
+	}
+
+	public static getInstance(): HostManager {
+		if (HostManager.instance === null) {
+			HostManager.instance = new HostManager();
+		}
+		return HostManager.instance;
+	}
+
+	public getShadowRoot(): ShadowRoot | HTMLDivElement {
+		if (process.env.MODE === 'production') {
+			if (this.shadowRoot === null || this.shadowRoot === undefined) {
+				this.shadowRoot = this.host.attachShadow({ mode: 'open' });
+			}
+		}
+		return this.shadowRoot ?? this.host;
+	}
+
+	public destroy(): void {
+		HostManager.instance = null;
+	}
+}
