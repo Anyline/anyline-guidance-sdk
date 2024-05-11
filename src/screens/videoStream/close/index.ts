ae15a69cdcea6c996c28425ec32e64a7ca9a47f8
Aditya Singh <aditya.singh@anyline.com>
2024-05-11 19:00:05 +0200

    CAPI-217(chore): show back icon in video screen and adjust css to make instructions responsive

--- src/screens/videoStream/close/index.ts
+++ src/screens/videoStream/close/index.ts
@@ -3,14 +3,15 @@ import FileInputManager from '../../../modules/FileInputManager';
 import Router from '../../../modules/Router';
 import StreamManager from '../../../modules/StreamManager';
 import VideoManager from '../../../modules/VideoManager';
+import commonCSS from '../../onboardingInstructions/closeSDKButton/index.module.css';
 import css from './index.module.css';
 
 export default function createCloseElement(): HTMLButtonElement {
 	const button = document.createElement('button');
-	button.className = css.button;
+	button.className = commonCSS.button;
 	button.id = 'closeButton';
 	button.setAttribute('data-test-id', 'closeButton');
-	button.innerHTML = `<div class=${css.buttonInner}><div>&#x2715;</div></div>`;
+	button.innerHTML = `<div class=${css.buttonInner}><div>&#10132;</div></div>`;
 
 	button.onclick = () => {
 		const videoStreamScreenManager = VideoStreamScreen.getInstance();
