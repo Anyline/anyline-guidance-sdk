a9b23fda3ea61b81a53cfbd254c1ca35e3646eaf
Aditya Singh <aditya.singh@anyline.com>
2024-05-12 18:33:35 +0200

    CAPI-217(chore): make onboarding instructions responsive

--- src/screens/onboardingInstructions/instructionsBody/index.ts
+++ src/screens/onboardingInstructions/instructionsBody/index.ts
@@ -1,17 +1,25 @@
 import createdemoImage from './demoInstructionsImage';
 import createHeadline from './headline';
 import createInstructions from './instructions';
+import css from './index.module.css';
 
 export default function createInstructionsBody(): HTMLDivElement {
 	const instructionBodyWrapper = document.createElement('div');
 
 	const headline = createHeadline();
+
+	const instructionsBody = document.createElement('div');
+
+	instructionsBody.className = css.instructionsBody;
+
 	const instructions = createInstructions();
 	const demoImage = createdemoImage();
 
+	instructionsBody.appendChild(instructions);
+	instructionsBody.appendChild(demoImage);
+
 	instructionBodyWrapper.appendChild(headline);
-	instructionBodyWrapper.appendChild(instructions);
-	instructionBodyWrapper.appendChild(demoImage);
+	instructionBodyWrapper.appendChild(instructionsBody);
 
 	return instructionBodyWrapper;
 }
