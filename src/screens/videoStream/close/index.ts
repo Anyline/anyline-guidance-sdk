1934e4ddb3d267d0fc15e4efbb9c92f9940456e4
Aditya Singh <aditya.singh@anyline.com>
2024-05-14 12:53:10 +0200

    CAPI-219(chore): closeSDK from video stream screen if no onboarding instructions screen was shown when sdk was initialised

--- src/screens/videoStream/close/index.ts
+++ src/screens/videoStream/close/index.ts
@@ -1,3 +1,5 @@
+import closeSDK from '../../../lib/closeSDK';
+import LocalStorageManager from '../../../modules/LocalStorageManager';
 import Router from '../../../modules/Router';
 import commonCSS from '../../onboardingInstructions/closeSDKButton/index.module.css';
 import css from './index.module.css';
@@ -10,6 +12,16 @@ export default function createCloseElement(): HTMLButtonElement {
 	button.innerHTML = `<div class=${css.buttonInner}><div>&#10132;</div></div>`;
 
 	button.onclick = () => {
+		const localStorageManager = LocalStorageManager.getInstance();
+
+		const timesShown =
+			localStorageManager.getTimesOnboardingInstructionsShown();
+
+		if (timesShown !== null && timesShown === 0) {
+			closeSDK();
+			return;
+		}
+
 		const routerManager = Router.getInstance();
 		routerManager.pop();
 	};
