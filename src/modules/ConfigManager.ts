904acdaff5f07beefd3b14dc391bea380ea6ddb9
Aditya Singh <aditya.singh@anyline.com>
2024-05-14 10:28:11 +0200

    CAPI-219(feat): add ConfigManager to manage user passed config to the sdk

---
+++ src/modules/ConfigManager.ts
@@ -0,0 +1,47 @@
+export interface Config {
+	onboardingInstructions: {
+		timesShown: number;
+	};
+}
+
+export default class ConfigManager {
+	private static instance: ConfigManager | null = null;
+	private readonly config: Config | null = null;
+
+	private constructor(config?: Config) {
+		if (config !== undefined) {
+			// use external library for schema validation if config increases
+			if (
+				typeof config !== 'object' ||
+				!Object.prototype.hasOwnProperty.call(
+					config,
+					'onboardingInstructions'
+				) ||
+				typeof config.onboardingInstructions !== 'object' ||
+				!Object.prototype.hasOwnProperty.call(
+					config.onboardingInstructions,
+					'timesShown'
+				) ||
+				typeof config.onboardingInstructions.timesShown !== 'number'
+			) {
+				throw new Error('Invalid config format');
+			}
+			this.config = config;
+		}
+	}
+
+	public static getInstance(config?: Config): ConfigManager {
+		if (ConfigManager.instance === null) {
+			ConfigManager.instance = new ConfigManager(config);
+		}
+		return ConfigManager.instance;
+	}
+
+	public getConfig(): Config | null {
+		return this.config;
+	}
+
+	public destory(): void {
+		ConfigManager.instance = null;
+	}
+}
