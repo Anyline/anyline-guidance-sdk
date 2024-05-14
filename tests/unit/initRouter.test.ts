9c872ea8ef4da42755bee1e07be51c47712452ed
Aditya Singh <aditya.singh@anyline.com>
2024-05-14 12:34:34 +0200

    CAPI-219(test): add test for initRouter

---
+++ tests/unit/initRouter.test.ts
@@ -0,0 +1,41 @@
+import initRouter from '../../src/lib/initRouter';
+import ConfigManager, { type Config } from '../../src/modules/ConfigManager';
+import LocalStorageManager from '../../src/modules/LocalStorageManager';
+
+describe('initRouter', () => {
+	const modal: HTMLDivElement = document.createElement('div');
+
+	afterEach(() => {
+		ConfigManager.getInstance().destroy();
+		LocalStorageManager.getInstance().destroy();
+		localStorage.clear();
+	});
+
+	it('should throw error when it is called with invalid config', () => {
+		const config = 'invalid config';
+
+		void expect(() => {
+			initRouter(modal, config as unknown as Config);
+		}).toThrow('Invalid config format');
+	});
+
+	it('should return onboardingInstructionsShown as false when it is called with timesShown = 0', () => {
+		const config: Config = {
+			onboardingInstructions: {
+				timesShown: 0,
+			},
+		};
+		const { onboardingInstructionsShown } = initRouter(modal, config);
+		void expect(onboardingInstructionsShown).toBe(false);
+	});
+
+	it('should return onboardingInstructionsShown as true when it is called with timesShown > 0', () => {
+		const config: Config = {
+			onboardingInstructions: {
+				timesShown: 1,
+			},
+		};
+		const { onboardingInstructionsShown } = initRouter(modal, config);
+		void expect(onboardingInstructionsShown).toBe(true);
+	});
+});
