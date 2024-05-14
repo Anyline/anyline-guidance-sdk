1650246b01cf52f79f4bbf77f4c9af370e264b79
Aditya Singh <aditya.singh@anyline.com>
2024-05-14 10:27:33 +0200

    CAPI-219(test): add test for ConfigManager

---
+++ tests/unit/ConfigManager.test.ts
@@ -0,0 +1,39 @@
+import ConfigManager, { type Config } from '../../src/modules/ConfigManager';
+
+describe('ConfigManager', () => {
+	it('can be initiated with config and without config', () => {
+		const configManager = ConfigManager.getInstance();
+		void expect(configManager.getConfig()).toEqual(null);
+		configManager.destory();
+
+		const config = { onboardingInstructions: { timesShown: 2 } };
+		const configManager2 = ConfigManager.getInstance(config);
+		void expect(configManager2.getConfig()).toEqual(config);
+		configManager2.destory();
+	});
+
+	it('throws error when config does not match expected type', () => {
+		const testCases = [
+			{ something: 'else' },
+			{ onboardingInstructions: 'not an object' },
+			{ onboardingInstructions: { timesShown: 'not a number' } },
+			{},
+			'test',
+		];
+
+		testCases.forEach(config => {
+			let errorOccurred = false;
+
+			try {
+				const configManager = ConfigManager.getInstance(
+					config as unknown as Config
+				);
+				configManager.destory();
+			} catch (error) {
+				errorOccurred = true;
+			}
+
+			void expect(errorOccurred).toBe(true);
+		});
+	});
+});
