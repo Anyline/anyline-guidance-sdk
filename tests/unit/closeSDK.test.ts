dbe6340322d3b349b8b39555ed1ddd4e94b5880d
Aditya Singh <aditya.singh@anyline.com>
2024-05-24 17:01:02 +0200

    CAPI-222(feat): disable and enable document body scroll when sdk is initialised and closed respectively

--- tests/unit/closeSDK.test.ts
+++ tests/unit/closeSDK.test.ts
@@ -6,6 +6,7 @@ import ImageManager from '../../src/modules/ImageManager';
 import HostManager from '../../src/modules/HostManager';
 import ConfigManager from '../../src/modules/ConfigManager';
 import LocalStorageManager from '../../src/modules/LocalStorageManager';
+import DocumentScrollController from '../../src/modules/DocumentScrollController';
 
 describe('closeSDK', () => {
 	it('removes host from DOM', async () => {
@@ -23,6 +24,7 @@ describe('closeSDK', () => {
 	it('remove all running instances of the SDK', () => {
 		const router = Router.getInstance();
 		const configManager = ConfigManager.getInstance();
+		const documentScrollController = DocumentScrollController.getInstance();
 		const imageManager = ImageManager.getInstance();
 		const localStorageManager = LocalStorageManager.getInstance();
 		const hostManager = HostManager.getInstance();
@@ -36,6 +38,18 @@ describe('closeSDK', () => {
 			.spyOn(configManager, 'destroy')
 			.mockImplementation(() => {});
 		void expect(configManagerSpy).not.toHaveBeenCalled();
+
+		const documentScrollControllerEnableScrollSpy = jest
+			.spyOn(documentScrollController, 'enableScroll')
+			.mockImplementation(() => {});
+		void expect(
+			documentScrollControllerEnableScrollSpy
+		).not.toHaveBeenCalled();
+
+		const documentScrollControllerSpy = jest
+			.spyOn(documentScrollController, 'destroy')
+			.mockImplementation(() => {});
+		void expect(documentScrollControllerSpy).not.toHaveBeenCalled();
 
 		const imageManagerSpy = jest
 			.spyOn(imageManager, 'destroy')
@@ -56,6 +70,10 @@ describe('closeSDK', () => {
 
 		void expect(routerSpy).toHaveBeenCalledTimes(1);
 		void expect(configManagerSpy).toHaveBeenCalledTimes(1);
+		void expect(
+			documentScrollControllerEnableScrollSpy
+		).toHaveBeenCalledTimes(1);
+		void expect(documentScrollControllerSpy).toHaveBeenCalledTimes(1);
 		void expect(imageManagerSpy).toHaveBeenCalledTimes(1);
 		void expect(localStorageManagerSpy).toHaveBeenCalledTimes(1);
 		void expect(hostManagerSpy).toHaveBeenCalledTimes(1);
