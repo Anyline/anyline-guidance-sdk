5f6d70624c089a971f2a307c9a0e96075a23e420
Aditya Singh <aditya.singh@anyline.com>
2024-05-24 10:01:31 +0200

    CAPI-222(chore): closeSDK only destroys top-level modules

--- src/lib/closeSDK.ts
+++ src/lib/closeSDK.ts
@@ -1,37 +1,23 @@
 import ConfigManager from '../modules/ConfigManager';
-import FileInputManager from '../modules/FileInputManager';
 import HostManager from '../modules/HostManager';
+import ImageChecker from '../modules/ImageChecker';
 import ImageManager from '../modules/ImageManager';
 import LocalStorageManager from '../modules/LocalStorageManager';
 import Router from '../modules/Router';
-import StreamManager from '../modules/StreamManager';
-import VideoManager from '../modules/VideoManager';
-import OnboardingScreen from '../screens/onboardingInstructions';
-import VideoStreamScreen from '../screens/videoStream';
 
 export default function closeSDK(): void {
-	const onboardingScreenManager = OnboardingScreen.getInstance();
-	const videoStreamScreenManager = VideoStreamScreen.getInstance();
-
 	const configManager = ConfigManager.getInstance();
-	const fileInputManager = FileInputManager.getInstance();
 	const hostManager = HostManager.getInstance();
 	const imageManager = ImageManager.getInstance();
+	const imageChecker = ImageChecker.getInstance();
 	const localStorageManager = LocalStorageManager.getInstance();
 	const routerManager = Router.getInstance();
-	const streamManager = StreamManager.getInstance();
-	const videoManager = VideoManager.getInstance();
-
-	onboardingScreenManager.destroy();
-	videoStreamScreenManager.destroy();
 
 	configManager.destroy();
-	fileInputManager.destroy();
 	imageManager.destroy();
+	imageChecker.destroy();
 	localStorageManager.destroy();
 	routerManager.destroy();
-	streamManager.destroy();
-	videoManager.destroy();
 
 	hostManager.destroy();
 }
--- tests/unit/closeSDK.test.ts
+++ tests/unit/closeSDK.test.ts
@@ -2,13 +2,8 @@ import '@testing-library/jest-dom';
 import closeSDK from '../../src/lib/closeSDK';
 import { waitFor } from '@testing-library/dom';
 import Router from '../../src/modules/Router';
-import StreamManager from '../../src/modules/StreamManager';
-import VideoManager from '../../src/modules/VideoManager';
-import FileInputManager from '../../src/modules/FileInputManager';
 import ImageManager from '../../src/modules/ImageManager';
-import VideoStreamScreen from '../../src/screens/videoStream';
 import HostManager from '../../src/modules/HostManager';
-import OnboardingScreen from '../../src/screens/onboardingInstructions';
 import ConfigManager from '../../src/modules/ConfigManager';
 import LocalStorageManager from '../../src/modules/LocalStorageManager';
 
@@ -21,20 +16,15 @@ describe('closeSDK', () => {
 
 		closeSDK();
 		await waitFor(async () => {
-			await expect(host).not.toBeInTheDocument();
+			void expect(host).not.toBeInTheDocument();
 		});
 	});
 
 	it('remove all running instances of the SDK', () => {
 		const router = Router.getInstance();
-		const streamManager = StreamManager.getInstance();
-		const videoManager = VideoManager.getInstance();
 		const configManager = ConfigManager.getInstance();
-		const fileInputManager = FileInputManager.getInstance();
 		const imageManager = ImageManager.getInstance();
 		const localStorageManager = LocalStorageManager.getInstance();
-		const videoStreamScreenManager = VideoStreamScreen.getInstance();
-		const onboardingScreenManager = OnboardingScreen.getInstance();
 		const hostManager = HostManager.getInstance();
 
 		const routerSpy = jest
@@ -42,25 +32,10 @@ describe('closeSDK', () => {
 			.mockImplementation(() => {});
 		void expect(routerSpy).not.toHaveBeenCalled();
 
-		const streamManagerSpy = jest
-			.spyOn(streamManager, 'destroy')
-			.mockImplementation(() => {});
-		void expect(streamManagerSpy).not.toHaveBeenCalled();
-
-		const videoManagerSpy = jest
-			.spyOn(videoManager, 'destroy')
-			.mockImplementation(() => {});
-		void expect(videoManagerSpy).not.toHaveBeenCalled();
-
 		const configManagerSpy = jest
 			.spyOn(configManager, 'destroy')
 			.mockImplementation(() => {});
 		void expect(configManagerSpy).not.toHaveBeenCalled();
-
-		const fileInputManagerSpy = jest
-			.spyOn(fileInputManager, 'destroy')
-			.mockImplementation(() => {});
-		void expect(fileInputManagerSpy).not.toHaveBeenCalled();
 
 		const imageManagerSpy = jest
 			.spyOn(imageManager, 'destroy')
@@ -72,16 +47,6 @@ describe('closeSDK', () => {
 			.mockImplementation(() => {});
 		void expect(localStorageManagerSpy).not.toHaveBeenCalled();
 
-		const videoStreamScreenManagerSpy = jest
-			.spyOn(videoStreamScreenManager, 'destroy')
-			.mockImplementation(() => {});
-		void expect(videoStreamScreenManagerSpy).not.toHaveBeenCalled();
-
-		const onboardingScreenManagerSpy = jest
-			.spyOn(onboardingScreenManager, 'destroy')
-			.mockImplementation(() => {});
-		void expect(onboardingScreenManagerSpy).not.toHaveBeenCalled();
-
 		const hostManagerSpy = jest
 			.spyOn(hostManager, 'destroy')
 			.mockImplementation(() => {});
@@ -90,14 +55,9 @@ describe('closeSDK', () => {
 		closeSDK();
 
 		void expect(routerSpy).toHaveBeenCalledTimes(1);
-		void expect(streamManagerSpy).toHaveBeenCalledTimes(1);
-		void expect(videoManagerSpy).toHaveBeenCalledTimes(1);
 		void expect(configManagerSpy).toHaveBeenCalledTimes(1);
-		void expect(fileInputManagerSpy).toHaveBeenCalledTimes(1);
 		void expect(imageManagerSpy).toHaveBeenCalledTimes(1);
 		void expect(localStorageManagerSpy).toHaveBeenCalledTimes(1);
-		void expect(videoStreamScreenManagerSpy).toHaveBeenCalledTimes(1);
-		void expect(onboardingScreenManagerSpy).toHaveBeenCalledTimes(1);
 		void expect(hostManagerSpy).toHaveBeenCalledTimes(1);
 	});
 });
