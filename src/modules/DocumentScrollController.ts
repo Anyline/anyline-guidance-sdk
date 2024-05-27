48ce92d10b75f9c63956800af5a6debdae8f215e
Aditya Singh <aditya.singh@anyline.com>
2024-05-27 14:19:26 +0200

    CAPI-222(fix): re-append body style class when DocumentScrollController.disableScroll is called

--- src/modules/DocumentScrollController.ts
+++ src/modules/DocumentScrollController.ts
@@ -1,13 +1,6 @@
-const style = document.createElement('style');
-style.textContent = `
-  .hideOverflow {
-    overflow: hidden;
-  }
-`;
-document.head.append(style);
-
 export default class DocumentScrollController {
 	private static instance: DocumentScrollController | null = null;
+	private readonly style: HTMLStyleElement = document.createElement('style');
 
 	public static getInstance(): DocumentScrollController {
 		if (DocumentScrollController.instance === null) {
@@ -17,18 +10,24 @@ export default class DocumentScrollController {
 	}
 
 	public disableScroll(): void {
+		this.style.textContent = `
+    .hideOverflow {
+      overflow: hidden;
+    }
+  `;
+		document.head.append(this.style);
 		document.body.classList.add('hideOverflow');
 	}
 
 	public enableScroll(): void {
 		document.body.classList.remove('hideOverflow');
+		if (this.style.parentNode != null) {
+			this.style.parentNode.removeChild(this.style);
+		}
+		document.body.classList.remove('hideOverflow');
 	}
 
 	public destroy(): void {
-		document.body.classList.remove('hideOverflow');
-		if (style.parentNode != null) {
-			style.parentNode.removeChild(style);
-		}
 		DocumentScrollController.instance = null;
 	}
 }
