5447091e64c73ec118867de24bf25dcf453f400b
Aditya Singh <aditya.singh@anyline.com>
2024-05-29 08:00:16 +0200

    CAPI-231(chore): adjust instructions style to make demo gif fully visible

--- src/screens/onboardingInstructions/instructionsBody/instructions.ts
+++ src/screens/onboardingInstructions/instructionsBody/instructions.ts
@@ -4,7 +4,7 @@ export default function createInstructions(): HTMLDivElement {
 	const instructionsWrapper = document.createElement('div');
 	instructionsWrapper.className = css.instructionsWrapper;
 
-	const instructions = document.createElement('p');
+	const instructions = document.createElement('div');
 
 	instructions.innerHTML =
 		'<p>As the first step, an overlay will guide you in placing the tire.</p> <p>Once positioned correctly, keep still and tap to open the camera for capture.</p>';
