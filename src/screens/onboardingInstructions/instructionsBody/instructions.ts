bc6718bff651d34ec9279202cad5895d42143a12
Aditya Singh <aditya.singh@anyline.com>
2024-06-03 09:56:42 +0200

    CAPI-233(feat): add component instructions list and to reuse code in onboarding instructions and video stream screen

--- src/screens/onboardingInstructions/instructionsBody/instructions.ts
+++ src/screens/onboardingInstructions/instructionsBody/instructions.ts
@@ -1,13 +1,14 @@
+import createInstructionsList from '../../../components/instructionsList';
 import css from './index.module.css';
 
 export default function createInstructions(): HTMLDivElement {
 	const instructionsWrapper = document.createElement('div');
 	instructionsWrapper.className = css.instructionsWrapper;
 
-	const instructions = document.createElement('div');
-
-	instructions.innerHTML =
-		'<p>As the first step, an overlay will guide you in placing the tire.</p> <p>Once positioned correctly, keep still and tap to open the camera for capture.</p>';
+	const instructions = createInstructionsList(`
+  <li>Place the tire within the <b>overlay</b></li>
+  <li><b>Keep still</b> and <b>open the camera</b></li>
+`);
 
 	instructionsWrapper.appendChild(instructions);
 
