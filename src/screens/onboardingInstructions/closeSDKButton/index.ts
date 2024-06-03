7f06cccb58d81100e7db887ceb31c69cdca6995e
Aditya Singh <aditya.singh@anyline.com>
2024-06-03 08:33:57 +0200

    CAPI-233(feat): add new back arrow svg

--- src/screens/onboardingInstructions/closeSDKButton/index.ts
+++ src/screens/onboardingInstructions/closeSDKButton/index.ts
@@ -1,14 +1,21 @@
 import closeSDK from '../../../lib/closeSDK';
 import css from './index.module.css';
+import backArrow from './assets/backArrow.svg';
 
 export default function createCloseSDKButton(): HTMLButtonElement {
 	const button = document.createElement('button');
-	button.className = `${css.button} ${css.buttonPosition}`;
+	button.className = css.button;
 	button.setAttribute(
 		'data-testid',
 		'components-onboarding-instructions-close-sdk-button'
 	);
-	button.innerHTML = `<div class='${css.buttonInner} ${css.buttonInnerPosition}'><div>&#x2715;</div></div>`;
+
+	const buttonArrowImage = document.createElement('img');
+	buttonArrowImage.className = css.buttonArrowImage;
+	buttonArrowImage.src = backArrow;
+
+	button.appendChild(buttonArrowImage);
+
 	button.type = 'button';
 
 	button.onclick = () => {
