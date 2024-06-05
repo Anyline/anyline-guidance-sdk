77db2393fe824f1b7b62304e56bec6fd951bfabc
Aditya Singh <aditya.singh@anyline.com>
2024-06-05 12:11:10 +0200

    CAPI-233(feat): enable code splitting for esm build and lazy load demo gif to split the sdk main chunk from gif chunk

--- src/screens/videoStream/instruction/index.ts
+++ src/screens/videoStream/instruction/index.ts
@@ -8,7 +8,7 @@ export default function createInstructionElement(): HTMLDivElement {
 	const instruction =
 		createInstructionsList(`<li><b>Align</b> the tire within the <b>overlay</b></li>
   <li>Hold <b>steady</b></li>
-  <li><b>Open the camera</b> for capture</li>`);
+  <li>Tap Continue to <b>open the camera</b></li>`);
 
 	wrapper.appendChild(instruction);
 
