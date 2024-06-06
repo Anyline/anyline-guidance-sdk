848fca187572efacef512c756b4da614f421e13d
Aditya Singh <aditya.singh@anyline.com>
2024-06-06 16:19:11 +0200

    CAPI-235(test): manage config via setConfig from ConfigManager

--- tests/unit/init.test.ts
+++ tests/unit/init.test.ts
@@ -2,6 +2,6 @@ import init from '../../src/index';
 
 describe('init', () => {
 	it('rejects for unsupported devices', async () => {
-		await expect(init()).rejects.toThrow('Unsupported device');
+		await expect(init({})).rejects.toThrow('Unsupported device');
 	});
 });
