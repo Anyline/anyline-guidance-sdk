eef2f75f209208a8f40e7903b95f8f92266d1a64
Aditya Singh <aditya.singh@anyline.com>
2024-06-06 16:19:11 +0200

    CAPI-235(test): manage config via setConfig from ConfigManager

--- tests/unit/ConfigManager.test.ts
+++ tests/unit/ConfigManager.test.ts
@@ -1,42 +1,100 @@
-import ConfigManager, {
-	type Config,
-} from '../../src/modules/ConfigManager/ConfigManager';
+import ConfigManager from '../../src/modules/ConfigManager/ConfigManager';
 
 describe('ConfigManager', () => {
-	it('can be initiated with config and without config', () => {
-		const configManager = ConfigManager.getInstance();
-		void expect(configManager.getConfig()).toEqual(null);
-		configManager.destroy();
+	let configManager: ConfigManager;
 
-		const config = { onboardingInstructions: { timesShown: 2 } };
-		const configManager2 = ConfigManager.getInstance(config);
-		void expect(configManager2.getConfig()).toEqual(config);
-		configManager2.destroy();
+	beforeEach(() => {
+		configManager = ConfigManager.getInstance();
 	});
 
-	it('throws error when config does not match expected type', () => {
+	afterEach(() => {
+		configManager.destroy();
+	});
+
+	it('should throw error if config is invalid', () => {
 		const testCases = [
-			{ something: 'else' },
-			{ onboardingInstructions: 'not an object' },
-			{ onboardingInstructions: { timesShown: 'not a number' } },
-			{ onboardingInstructions: { timesShown: -3 } },
-			{},
-			'test',
+			{
+				config: {
+					onboardingInstructions: '',
+				},
+				expected: '"onboardingInstructions" must be of type object',
+			},
+			{
+				config: {
+					onboardingInstructions: {},
+				},
+				expected: '"onboardingInstructions.timesShown" is required',
+			},
+			{
+				config: {
+					onboardingInstructions: {
+						timesShown: 3,
+					},
+					invalidKey: 'test',
+				},
+				expected: '"invalidKey" is not allowed',
+			},
+			{
+				config: 'asd',
+				expected: '"value" must be of type object',
+			},
+			{
+				config: 0,
+				expected: '"value" must be of type object',
+			},
+			{
+				config: null,
+				expected: '"value" must be of type object',
+			},
 		];
 
-		testCases.forEach(config => {
-			let errorOccurred = false;
+		testCases.forEach(testCase => {
+			const { config, expected } = testCase;
 
-			try {
-				const configManager = ConfigManager.getInstance(
-					config as unknown as Config
-				);
-				configManager.destroy();
-			} catch (error) {
-				errorOccurred = true;
-			}
+			const oldConfig = configManager.getConfig();
+			void expect(oldConfig).toStrictEqual(null);
+			void expect(() => {
+				// @ts-expect-error: purposefully pass invalid config
+				configManager.setConfig(config);
+			}).toThrow(expected);
+			const newConfig = configManager.getConfig();
+			void expect(newConfig).toStrictEqual(null);
+			configManager.destroy();
+		});
+	});
 
-			void expect(errorOccurred).toBe(true);
+	it('should return config for a valid config', () => {
+		const testCases = [
+			{
+				config: {
+					onboardingInstructions: {
+						timesShown: 3,
+					},
+				},
+				expected: {
+					onboardingInstructions: {
+						timesShown: 3,
+					},
+				},
+			},
+			{
+				config: {},
+				expected: {},
+			},
+		];
+
+		testCases.forEach(testCase => {
+			const configManager2 = ConfigManager.getInstance();
+			const { config, expected } = testCase;
+
+			const oldConfig = configManager2.getConfig();
+			void expect(oldConfig).toStrictEqual(null);
+			void expect(() => {
+				configManager2.setConfig(config);
+			}).not.toThrow();
+			const newConfig = configManager2.getConfig();
+			void expect(newConfig).toStrictEqual(expected);
+			configManager2.destroy();
 		});
 	});
 });
--- tests/unit/initRouter.test.ts
+++ tests/unit/initRouter.test.ts
@@ -1,7 +1,5 @@
 import initRouter from '../../src/lib/initRouter';
-import ConfigManager, {
-	type Config,
-} from '../../src/modules/ConfigManager/ConfigManager';
+import ConfigManager from '../../src/modules/ConfigManager/ConfigManager';
 import LocalStorageManager from '../../src/modules/LocalStorageManager';
 
 describe('initRouter', () => {
@@ -13,16 +11,8 @@ describe('initRouter', () => {
 		localStorage.clear();
 	});
 
-	it('should throw error when it is called with invalid config', () => {
-		const config = 'invalid config';
-
-		void expect(() => {
-			initRouter(modal, config as unknown as Config);
-		}).toThrow('Invalid config format');
-	});
-
-	it('should return onboardingInstructionsShown as false when it is called with timesShown = 0', () => {
-		const config: Config = {
+	it('should not show onboarding instructions if timeshown is 0', () => {
+		const config = {
 			onboardingInstructions: {
 				timesShown: 0,
 			},
@@ -31,8 +21,8 @@ describe('initRouter', () => {
 		void expect(onboardingInstructionsShown).toBe(false);
 	});
 
-	it('should return onboardingInstructionsShown as true when it is called with timesShown > 0', () => {
-		const config: Config = {
+	it('should show onboarding instructions if timeshown is greater than 1', () => {
+		const config = {
 			onboardingInstructions: {
 				timesShown: 1,
 			},
