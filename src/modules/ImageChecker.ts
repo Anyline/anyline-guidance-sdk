6c17aea41c2e8e7742e24ddc0268b4fdcbae392b
Aditya Singh <aditya.singh@anyline.com>
2024-06-07 09:21:27 +0200

    CAPI-235(chore): use ImageChecker.onBlobSet to proceed in PreProcessingScreen with the blob

--- src/modules/ImageChecker.ts
+++ src/modules/ImageChecker.ts
@@ -2,9 +2,15 @@ import preProcessImage from '../lib/preProcessImage';
 
 export default class ImageChecker {
 	private static instance: ImageChecker | null = null;
-	public blob: Blob | null = null;
+	private blob: Blob | null = null;
+	private readonly blobSetPromise: Promise<Blob>;
+	private blobSetResolve: ((blob: Blob) => void) | null = null;
 
-	private constructor() {}
+	private constructor() {
+		this.blobSetPromise = new Promise<Blob>(resolve => {
+			this.blobSetResolve = resolve;
+		});
+	}
 
 	public static getInstance(): ImageChecker {
 		if (ImageChecker.instance === null) {
@@ -14,12 +20,18 @@ export default class ImageChecker {
 	}
 
 	public setImageBlob(blob: Blob): void {
-		this.blob = blob;
+		if (this.blob == null) {
+			this.blob = blob;
+			if (this.blobSetResolve != null) {
+				this.blobSetResolve(this.blob);
+			}
+		}
 	}
 
-	public getImageBlob(): Blob {
-		if (this.blob === null) throw new Error('No image found');
-		return this.blob;
+	public onBlobSet(callback: (blob: Blob) => Promise<void>): void {
+		void this.blobSetPromise.then(async () => {
+			if (this.blob !== null) await callback(this.blob);
+		});
 	}
 
 	public async isImageQualityGood(): Promise<boolean> {
@@ -35,5 +47,6 @@ export default class ImageChecker {
 
 	public destroy(): void {
 		ImageChecker.instance = null;
+		this.blob = null;
 	}
 }
